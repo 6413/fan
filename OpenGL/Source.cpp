8db042c265370f6879e4e7a4b78d89383b87873b
6413 <56801084+6413@users.noreply.github.com>
2020-10-11 19:12:37 +0300

    Updated makefile

--- OpenGL/Source.cpp
+++ OpenGL/Source.cpp
@@ -6,8 +6,10 @@ int main() {
 
 	fan_3d::terrain_generator tg("grass.jpg", 150, 10, 5);
 
+	fan_3d::camera.set_position(fan::vec3(500, 20, 500));
+
 	fan::window_loop(0, [&] {
-		fan_3d::camera.move(true, 100);
+		fan_3d::camera.move(true, 300);
 		tg.draw();
 	});
 
