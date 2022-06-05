c3d54ee4dba39df86e17a95351d376b68b8e1dea
8347r1c3 <8347r1c3@cock.li>
2022-06-05 14:39:50 +0300

    audio update

--- include/fan/audio/backend/unix/linux/alsa/a.h
+++ include/fan/audio/backend/unix/linux/alsa/a.h
@@ -1,78 +1,105 @@
-#define MA_NO_DECODING
-#define MA_NO_ENCODING
-#define MINIAUDIO_IMPLEMENTATION
-#include "lib.h"
+#include <alsa/asoundlib.h>
 
 struct audio_t {
   _audio_common_t common;
 
-  ma_context context;
-  ma_device device;
+  f32_t Volume;
+
+  TH_id_t thid;
+  snd_pcm_t *snd_pcm;
 };
 
-namespace {
-  void BackendDataCallback(ma_device* Device, void* Output, const void* Input, ma_uint32 FrameCount) {
-    #if fan_debug >= 0
-      if (FrameCount != _constants::CallFrameCount) {
-        fan::throw_error("fan_debug");
+void *_audio_thread_func(void *p) {
+  audio_t *audio = (audio_t *)p;
+  while(1){
+    f32_t frames[_constants::CallFrameCount * _constants::ChannelAmount] = {0};
+
+    _DataCallback(&audio->common, frames);
+    snd_pcm_sframes_t rframes = snd_pcm_writei(audio->snd_pcm, frames, _constants::CallFrameCount);
+    if (rframes != _constants::CallFrameCount) {
+      switch(rframes){
+        case -EPIPE:{
+          int r = snd_pcm_recover(audio->snd_pcm, -EPIPE, 0);
+          if(r != 0){
+            fan::throw_error("failed to recover");
+          }
+          break;
+        }
+        default:{
+          fan::throw_error("snd_pcm_writei");
+        }
       }
-    #endif
-    audio_t *audio = (audio_t *)Device->pUserData;
-    _DataCallback(&audio->common, (f32_t *)Output);
+    }
+
+    for(uint32_t i = 0; i < _constants::CallFrameCount * _constants::ChannelAmount; i++){
+      frames[i] *= audio->Volume;
+    }
   }
+  return 0;
 }
 
 void audio_close(audio_t* audio) {
-  ma_device_uninit(&audio->device);
-  ma_context_uninit(&audio->context);
+  fan::throw_error("TODO not yet");
 
   _audio_common_close(&audio->common);
 }
 void audio_open(audio_t *audio, uint32_t GroupAmount) {
   _audio_common_open(&audio->common, GroupAmount);
 
-  ma_result r;
-  if ((r = ma_context_init(NULL, 0, NULL, &audio->context)) != MA_SUCCESS) {
-    fan::throw_error("error" + ::std::to_string(r));
+  audio->Volume = 1;
+
+  int ierr = snd_pcm_open(&audio->snd_pcm, "default", SND_PCM_STREAM_PLAYBACK, 0);
+  if(ierr < 0){
+    fan::throw_error("a");
   }
 
-  ma_device_config config = ma_device_config_init(ma_device_type_playback);
-  config.playback.format = ma_format_f32;
-  config.playback.channels = _constants::ChannelAmount;
-  config.sampleRate = _constants::opus_decode_sample_rate;
-  config.dataCallback = BackendDataCallback;
-  config.pUserData = audio;
-  config.periodSizeInFrames = _constants::CallFrameCount;
+  snd_pcm_hw_params_t *params;
+  snd_pcm_hw_params_alloca(&params);
+  snd_pcm_hw_params_any(audio->snd_pcm, params);
 
-  if ((r = ma_device_init(&audio->context, &config, &audio->device)) != MA_SUCCESS) {
-    fan::throw_error("ma_device_init" + ::std::to_string(r));
+  if((ierr = snd_pcm_hw_params_set_access(audio->snd_pcm, params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0){
+    fan::throw_error("a");
+  }
+
+  if((ierr = snd_pcm_hw_params_set_format(audio->snd_pcm, params, SND_PCM_FORMAT_FLOAT_LE)) < 0){
+    fan::throw_error("a");
+  }
+
+  if((ierr = snd_pcm_hw_params_set_channels(audio->snd_pcm, params, _constants::ChannelAmount)) < 0){
+    fan::throw_error("a");
+  }
+
+  if((ierr = snd_pcm_hw_params_set_rate(audio->snd_pcm, params, _constants::opus_decode_sample_rate, 0)) < 0){
+    fan::throw_error("a");
+  }
+
+  if((ierr = snd_pcm_hw_params_set_periods(audio->snd_pcm, params, 3, 0)) < 0){
+    fan::throw_error("w");
+  }
+
+  if((ierr = snd_pcm_hw_params_set_period_size(audio->snd_pcm, params, _constants::CallFrameCount, 0)) < 0){
+    fan::throw_error("a");
+  }
+
+  if((ierr = snd_pcm_hw_params(audio->snd_pcm, params) < 0)){
+    fan::throw_error("a");
   }
 }
 
 void audio_stop(audio_t* audio) {
-  if (ma_device_stop(&audio->device) != MA_SUCCESS) {
-    fan::throw_error("audio_stop");
-  }
+  fan::throw_error("TODO not yet");
 }
 void audio_start(audio_t* audio) {
-  if (ma_device_start(&audio->device) != MA_SUCCESS) {
-    fan::throw_error("audio_start");
-  }
+  audio->thid = TH_open((void *)_audio_thread_func, audio);
 }
 
-void audio_stop_group(audio_t* audio, uint32_t GroupID) {
+void audio_stop_group(audio_t *audio, uint32_t GroupID) {
 
 }
 
-void audio_set_volume(audio_t* audio, f32_t Volume) {
-  if (ma_device_set_master_volume(&audio->device, Volume) != MA_SUCCESS) {
-    fan::throw_error("audio_set_volume");
-  }
+void audio_set_volume(audio_t *audio, f32_t Volume) {
+  audio->Volume = Volume;
 }
-f32_t audio_get_volume(audio_t* audio) {
-  f32_t Volume;
-  if (ma_device_get_master_volume(&audio->device, &Volume) != MA_SUCCESS) {
-    fan::throw_error("audio_get_volume");
-  }
-  return Volume;
+f32_t audio_get_volume(audio_t *audio) {
+  return audio->Volume;
 }
