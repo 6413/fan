f8054ca12f5131628e94b2145701d1bdad1183bc
6413 <56801084+6413@users.noreply.github.com>
2022-11-27 22:47:14 +0200

    up

--- Source.cpp
+++ Source.cpp
@@ -2,25 +2,19 @@
 #include <fan/types/types.h>
 #include <fan/types/vector.h>
 
-struct common {
-	inline static int x = 0;
-	constexpr bool f() { x++; return 0; }
-};
-
-struct protocol_c {
-	constexpr protocol_c(bool) {}
-	
-};
-
-#define make_p(n) protocol_c n{f()};
-
-struct protocol : common {
-	
-	make_p(a);
-	make_p(b);
-};
-
 int main(){
-	protocol p;
-	fan::print(p.x);
+	uint32_t* v = (uint32_t*)malloc(2048 * 2048 * 4);
+	fan::time::clock c;
+	c.start();
+	uint32_t i = 0;
+	for (; i < 1; i++) {
+		memset(v, 5, 2048 * 2048 * 4);
+	}
+	fan::print(c.elapsed() / i);
+	for (i = 0; i < 2048 * 2048 ; i++) {
+		if (v[i] != 0x05050505) {
+			fan::print(i, v[i]);
+			break;
+		}
+	}
 }
--- examples/compute_shader.cpp
+++ examples/compute_shader.cpp
@@ -29,7 +29,7 @@ int main() {
 
 	fan::vulkan::descriptor_t<ds_properties.size()> descriptor;
 
-	uint32_t buffer_size = 10000;
+	uint32_t buffer_size = 2048 * 2048 * 4;
 
 	fan::vulkan::core::memory_t memory;
 
@@ -37,8 +37,8 @@ int main() {
 		context,
 		buffer_size,
 		VK_BUFFER_USAGE_STORAGE_BUFFER_BIT,
-		VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
-		//VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT | VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT, // ?
+		//VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+		VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT | VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT, // ?
 		// faster ^? depends about buffer size maxMemoryAllocationCount maybe
 		memory.buffer,
 		memory.device_memory
@@ -47,7 +47,7 @@ int main() {
 	ds_properties[0].binding = 0;
 	ds_properties[0].type = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
 	ds_properties[0].flags = VK_SHADER_STAGE_COMPUTE_BIT;
-	ds_properties[0].range = VK_WHOLE_SIZE;
+	ds_properties[0].range = buffer_size; // VK_WHOLE_SIZE
 	ds_properties[0].buffer = memory.buffer;
 	ds_properties[0].dst_binding = 0;
 
@@ -62,21 +62,28 @@ int main() {
 	loco_t::compute_shader_t compute_shader(&pile->loco, p);
 
 	void* data;
-	fan::vulkan::validate(vkMapMemory(pile->loco.get_context()->device, memory.device_memory, 0, 10000, 0, (void**)&data));
+	fan::vulkan::validate(vkMapMemory(pile->loco.get_context()->device, memory.device_memory, 0, buffer_size, 0, (void**)&data));
 
-	context->begin_compute_shader();
+	fan::time::clock c;
+	c.start();
+	uint32_t i = 0;
+	for (; i < 1000; i++) {
+		context->begin_compute_shader();
 
-	compute_shader.execute(&pile->loco, fan::vec3(5, 1, 1));
+		compute_shader.execute(&pile->loco, fan::vec3(16, 64, 1));
 
-	context->end_compute_shader();
+		context->end_compute_shader();
 
-	compute_shader.wait_finish(&pile->loco);
+		compute_shader.wait_finish(&pile->loco);
+	}
 
-	for (uint32_t i = 0; i < 5 * 1 * 1 * 128 * 1 * 1; ++i) {
-		if (i && ((uint32_t*)data)[i] == 0) {
+	fan::print(c.elapsed() / i);
+
+	for (i = 0; i < buffer_size / 4; ++i) {
+		if (((uint32_t*)data)[i] != 5) {
+			fan::print(i, ((uint32_t*)data)[i]);
 			break;
 		}
-		fan::print(i, ((uint32_t*)data)[i]);
 	}
 
 	return 0;
