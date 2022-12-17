eab824a722cb31dca8792ceff873e69407d10c3c
6413 <56801084+6413@users.noreply.github.com>
2022-12-17 16:37:13 +0200

    up

--- include/fan/graphics/opengl/2D/objects/yuv420p.h
+++ include/fan/graphics/opengl/2D/objects/yuv420p.h
@@ -49,7 +49,7 @@ struct sb_sprite_name {
       loco_t::image_t::load_properties_t lp;
       lp.format = fan::opengl::GL_RED; 
       lp.internal_format = fan::opengl::GL_RED; 
-      lp.visual_output = GL_CLAMP_TO_EDGE;
+      lp.visual_output = fan::opengl::GL_CLAMP_TO_EDGE;
       // can be buggy
       lp.filter = fan::opengl::GL_LINEAR;
                                  
@@ -121,7 +121,7 @@ struct sb_sprite_name {
     lp.format = fan::opengl::GL_RED;
     lp.internal_format = fan::opengl::GL_RED;
     lp.filter = fan::opengl::GL_LINEAR;
-    lp.visual_output = GL_CLAMP_TO_EDGE;
+    lp.visual_output = fan::opengl::GL_CLAMP_TO_EDGE;
                                  
     fan::webp::image_info_t ii; 
                                
