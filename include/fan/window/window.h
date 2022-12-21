f224a0afd35bb9aa1f9499c632dd29d645838373
6413 <56801084+6413@users.noreply.github.com>
2022-12-21 17:41:49 +0200

    Update window.h

--- include/fan/window/window.h
+++ include/fan/window/window.h
@@ -52,6 +52,7 @@
 	#include <X11/keysym.h>
 	#include <X11/XKBlib.h>
   #include <X11/Xcursor/Xcursor.h>
+  #include <X11/extensions/Xfixes.h>
   #include <X11/cursorfont.h>
   #include <X11/Xutil.h>
 
