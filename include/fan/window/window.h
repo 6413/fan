f80de867c2bec2f69a0a96cbd323381ca3173e87
6413 <56801084+6413@users.noreply.github.com>
2022-12-25 03:56:30 +0200

    up

--- include/fan/window/window.h
+++ include/fan/window/window.h
@@ -589,6 +589,7 @@ namespace fan {
 		fan::vec2i m_previous_mouse_position;
 
     fan::vec2i m_mouse_motion;
+    fan::vec2i initial_motion = 0;
 
 		fan::color m_background_color;
 
--- src/fan/window/window.cpp
+++ src/fan/window/window.cpp
@@ -535,8 +535,7 @@ fan::window_t::window_t(const fan::vec2i& window_size, const fan::string& name, 
   m_reserved_flags = 0;
   m_focused = true;
   m_event_flags = 0;
-  const fan::vec2i center = fan::sys::get_screen_resolution() / 2;
-  m_mouse_motion = center;
+  m_mouse_motion = 0;
 
   if (flag_values.m_size_mode == fan::window_t::mode::not_set) {
     flag_values.m_size_mode = fan::window_t::default_size_mode;
@@ -1388,7 +1387,8 @@ uint32_t fan::window_t::handle_events() {
       it = it.Next(&m_mouse_motion_callback);
     }
     const fan::vec2i center = fan::sys::get_screen_resolution() / 2;
-    m_mouse_motion = center;
+    initial_motion = center;
+    m_mouse_motion = 0;
     m_average_motion = 0;
     call_mouse_motion_cb = false;
   }
@@ -2087,7 +2087,8 @@ uint32_t fan::window_t::handle_events() {
         window->m_previous_mouse_position = window->m_mouse_position;
 
         window->m_mouse_position = position;
-        m_mouse_motion = fan::vec2i(event.xmotion.x_root, event.xmotion.y_root) - center;
+        m_mouse_motion = (initial_motion) - center;
+        initial_motion = fan::vec2i(event.xmotion.x_root, event.xmotion.y_root);
         m_average_motion += m_mouse_motion;
         call_mouse_motion_cb = true;
 
