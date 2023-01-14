5d558d16552b83336c813b2ff6ce2811aa204dea
6413 <56801084+6413@users.noreply.github.com>
2023-01-14 19:49:14 +0200

    up

--- examples/cpp/neural_network.cpp
+++ examples/cpp/neural_network.cpp
@@ -37,12 +37,16 @@ void shuffle(int* arr, uint32_t size) {
 
 static constexpr int n_inputs = 2;
 static constexpr int n_hidden_nodes = 2;
-static constexpr int n_outputs = 2;
-static constexpr int n_training_sets = 4;
+static constexpr int n_outputs = 1;
+static constexpr double multiplier = 100;
 
 int main() {
+  static constexpr uint32_t number_of_epochs = 1000000;
+  static constexpr auto num_of_samples = 100;
+
+
   // learning faster or slower
-  const double learning_rate = 0.5;
+  const double learning_rate = 1;
 
   double hidden_layer[n_hidden_nodes];
   double output_layer[n_outputs];
@@ -60,20 +64,21 @@ int main() {
   double hidden_weights[n_inputs][n_hidden_nodes];
   double output_weights[n_hidden_nodes][n_outputs];
 
-  double training_inputs[n_training_sets][n_inputs] = {
-    {10.0, 5.0},
-    {7.0, 3.0},
-    {5.0, 3.0},
-    {9.0, 2.0}
-  };
+  std::vector<fan::vec2d> training_inputs;
+  std::vector<double> training_outputs;
 
-  double training_outputs[n_training_sets][n_outputs] = {
-    {15.0},
-    {10.0},
-    {8.0},
-    {11.0}
-  };
+  std::vector<int> training_set_order;
 
+  for (uint32_t i = 0; i < 100; ++i) {
+    training_inputs.push_back(
+      fan::vec2d(
+        i / multiplier,
+        fan::random::value_i64(0, 10) / multiplier
+      )
+    );
+    training_outputs.push_back(training_inputs[i][0] + training_inputs[i][1]);
+    training_set_order.push_back(i);
+  }
 
   for (uint32_t i = 0; i < n_inputs; ++i) {
     for (uint32_t j = 0; j < n_hidden_nodes; ++j) {
@@ -93,17 +98,15 @@ int main() {
     output_layer_bias[i] = init_weights();
   }
 
-  int training_set_order[] = {
-    0, 1, 2, 3
-  };
-
-  uint32_t number_of_epochs = 10000;
-
-  fan::string str;
+  std::string str;
 
   // training
+
+  uint32_t n_training_sets = training_inputs.size();
+
   for (uint32_t epoch = 0; epoch < number_of_epochs; ++epoch) {
-    shuffle(training_set_order, n_training_sets);
+    bool once = false;
+    shuffle(training_set_order.data(), n_training_sets);
 
     for (uint32_t x = 0; x < n_training_sets; ++x) {
       uint32_t i = training_set_order[x];
@@ -126,15 +129,18 @@ int main() {
         output_layer[j] = sigmoid(activation);
       }
 
-      str += fan::format("input:{} {}\noutput:{}\nexpected output:{}\n\n", 
-        training_inputs[i][0], training_inputs[i][1], output_layer[0], training_outputs[i][0]);
+      if ((!once && !(epoch % (number_of_epochs / num_of_samples)) || epoch + 1 == number_of_epochs)) {
+        str.append(fan::format("input:{} + {}\noutput:{}\nexpected output:{}\n\n", 
+          training_inputs[i][0] * multiplier, training_inputs[i][1] * multiplier, output_layer[0] * multiplier, training_outputs[i] * multiplier).data());
+        once = true;
+      }
 
       // backpropagation
 
       double delta_output[n_outputs];
 
       for (uint32_t j = 0; j < n_outputs; ++j) {
-        double error = (training_outputs[i][j] - output_layer[j]);
+        double error = (training_outputs[i] - output_layer[j]);
         delta_output[j] = error * derivative_sigmoid(output_layer[j]);
       }
 
@@ -166,5 +172,35 @@ int main() {
     }
   }
 
-  fan::io::file::write("data", str, std::ios_base::binary);
+
+  //uint32_t i = 0;
+
+  double a = 58;
+  double b = 32;
+
+  a /= multiplier;
+  b /= multiplier;
+
+
+  for (uint32_t j = 0; j < n_hidden_nodes; ++j) {
+    double activation = hidden_layer_bias[j];
+
+    activation += a * hidden_weights[0][j];
+    activation += b * hidden_weights[1][j];
+
+    hidden_layer[j] = sigmoid(activation);
+  }
+
+  for (uint32_t j = 0; j < n_outputs; ++j) {
+    double activation = output_layer_bias[j];
+
+    for (uint32_t k = 0; k < n_hidden_nodes; ++k) {
+      activation += hidden_layer[k] * output_weights[k][j];
+    }
+    output_layer[j] = sigmoid(activation);
+  }
+
+  fan::print(fan::format("input:{} {}\noutput:{}\nexpected output:{}\n\n",
+    a * multiplier, b * multiplier, output_layer[0] * multiplier, a * multiplier + b * multiplier));
+  fan::io::file::write("data", str.data(), std::ios_base::binary);
 }
--- include/fan/types/fstring.h
+++ include/fan/types/fstring.h
@@ -111,7 +111,7 @@ namespace fan {
 		}
 
 		auto append(const basic_string& s) {
-			insert(end(), s);
+      insert(s.end(), s);
 			return *this;
 		}
 
