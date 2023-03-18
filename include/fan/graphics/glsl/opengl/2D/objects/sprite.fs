b8577f98d78bb858a30c2b8b160fa6b2da2bb64b
6413 <56801084+6413@users.noreply.github.com>
2023-03-18 12:28:18 +0200

    up

--- include/fan/graphics/glsl/opengl/2D/objects/sprite.fs
+++ include/fan/graphics/glsl/opengl/2D/objects/sprite.fs
@@ -13,6 +13,9 @@ uniform vec2 window_size;
 
 void main() {
   o_attachment0 = texture(_t00, texture_coordinate) * instance_color;
+  if (o_attachment0.a <= 0.5) {
+    discard;
+  }
   vec4 t = vec4(texture(_t01, gl_FragCoord.xy / window_size).rgb, 1);
   o_attachment0.rgb *= lighting_ambient + t.rgb;
 }
