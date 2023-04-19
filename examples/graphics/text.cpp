2145f652dab478833aec8e3c7e3436093133de14
6413 <56801084+6413@users.noreply.github.com>
2023-04-19 23:42:41 +0300

    Update text.cpp

--- examples/graphics/text.cpp
+++ examples/graphics/text.cpp
@@ -1,6 +1,8 @@
 #define _INCLUDE_TOKEN(p0, p1) <p0/p1>
 
-#define FAN_INCLUDE_PATH C:/libs/fan/include
+#ifndef FAN_INCLUDE_PATH
+  #define FAN_INCLUDE_PATH C:/libs/fan/include
+#endif
 #define fan_debug 0
 #include _INCLUDE_TOKEN(FAN_INCLUDE_PATH, fan/types/types.h)
 
