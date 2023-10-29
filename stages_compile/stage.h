1bc7a4d9301abd03b44da628f04aeda7dcfc538c
6413 <56801084+6413@users.noreply.github.com>
2023-10-29 03:52:46 +0200

    updated stage maker

--- stages_compile/stage.h
+++ stages_compile/stage.h
@@ -1,20 +1,8 @@
 struct stage {
-  lstd_defstruct(stage_fuel_station_t)
+  lstd_defstruct(stage0_t)
     #include _FAN_PATH(graphics/gui/stage_maker/preset.h)
 
-    static constexpr auto stage_name = "stage_fuel_station";
-    #include _PATH_QUOTE(stage_loader_path/stages_compile/stage_fuel_station.h)
-  };
-  lstd_defstruct(stage2_t)
-    #include _FAN_PATH(graphics/gui/stage_maker/preset.h)
-
-    static constexpr auto stage_name = "stage2";
-    #include _PATH_QUOTE(stage_loader_path/stages_compile/stage2.h)
-  };
-  lstd_defstruct(stage_hud_t)
-    #include _FAN_PATH(graphics/gui/stage_maker/preset.h)
-
-    static constexpr auto stage_name = "stage_hud";
-    #include _PATH_QUOTE(stage_loader_path/stages_compile/stage_hud.h)
+    static constexpr auto stage_name = "stage0";
+    #include _PATH_QUOTE(stage_loader_path/stages_compile/stage0.h)
   };
 };
