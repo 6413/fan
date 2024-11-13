adb4ee3cb17c194a22be4c2f7fdfcd8a073e47f4
6413 <56801084+6413@users.noreply.github.com>
2024-11-13 21:10:34 +0200

    updated dme syntax

--- fan/types/dme.h
+++ fan/types/dme.h
@@ -1,36 +1,43 @@
 #pragma once
 
+#include <cstdint>
 #pragma pack(push, 1)
 
-template <typename T, unsigned long long idx, const char* str, typename dt_t = void>
-struct wrapper_t : T {
+#ifndef __empty_struct
+  struct __empty_struct{};
+  #define __empty_struct __empty_struct
+#endif
+
+template<typename T, uintptr_t idx, const char* str, typename dt_t = void>
+struct __dme_t : T {
   using dt = dt_t;
-  static constexpr unsigned long long I = idx;
-  constexpr operator unsigned long long() const { return I; }
+  static constexpr uintptr_t I = idx;
+  constexpr operator uintptr_t() const { return I; }
   operator T& () { return *this; }
-  static constexpr unsigned long long AN() { return I; }
+  static constexpr uintptr_t AN() { return I; }
   const char* sn = str;
 };
 
 template<typename T>
 using return_type_of_t = decltype((*(T*)nullptr)());
 
-inline char dme_empty_string__[] = "";
+inline char __dme_empty_string[] = "";
 
-#define __dme2(varname) static constexpr inline char internalstr_##varname[] = #varname;\
-  wrapper_t<dme_type_t, __COUNTER__ - DME_INTERNAL__BEG - 1,  internalstr_##varname> varname
-#define __dme2_data(varname, data) private: \
- static inline constexpr char internalstr_##varname[] = #varname;\
- struct internal_##varname {data}; public: wrapper_t<dme_type_t, __COUNTER__ - DME_INTERNAL__BEG - 1,  internalstr_##varname, internal_##varname> varname
+#define __dme(varname, data) \
+  static inline constexpr char varname##_str[] = #varname; \
+  struct varname##_t {data}; \
+  __dme_t<dme_type_t, __COUNTER__ - DME_INTERNAL__BEG - 1,  varname##_str, varname##_t> varname
 
-
-template <typename main_t, typename T, unsigned long long index>
-struct base_t {
+template <typename main_t, uintptr_t index, typename T = __empty_struct>
+struct __dme_inherit_t{
   using dme_type_t = T;
-  constexpr auto* NA(unsigned long long I) const { return &((wrapper_t<dme_type_t, 0, dme_empty_string__> *)this)[I]; }
-  static constexpr unsigned long long GetMemberAmount() { return sizeof(main_t) / sizeof(dme_type_t); }
+  constexpr auto* NA(uintptr_t I) const { return &((__dme_t<dme_type_t, 0, __dme_empty_string> *)this)[I]; }
+  static constexpr uintptr_t GetMemberAmount() { return sizeof(main_t) / sizeof(dme_type_t); }
   static constexpr auto DME_INTERNAL__BEG = index;
 };
 
-#define dme_inherit__(main_t, type) base_t<main_t, type, __COUNTER__>
+#define __dme_inherit(main_t, ...) __dme_inherit_t<main_t, __COUNTER__, ##__VA_ARGS__>
+
 #pragma pack(pop)
+
+#undef __empty_struct
