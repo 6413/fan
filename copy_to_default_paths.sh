e363123a1bb99161fc021b46f21df1e348cc7314
7244 <65ad4d@gmail.com>
2024-11-13 17:01:43 +0000

    sh up

--- copy_to_default_paths.sh
+++ copy_to_default_paths.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
-#requires sudo
-cp -r lib/fan/ /usr/local/lib/
+
+fan_include_path=/usr/local/include/fan
+fan_lib_path=/usr/local/lib/fan
+
+cerr () {
+  if [ $? -ne 0 ]; then
+    echo "command failed. exiting."
+    exit 1
+  fi
+}
+
+delete_link () {
+  if [ $# -ne 1 ]; then
+    echo "delete_link needs path"
+    exit 1
+  fi
+
+  if sudo [ -e $1 ] || sudo [ -L $1 ]; then
+    if sudo [ ! -L $1 ]; then
+      echo "delete_link: $1 exists but its not a symbolic link"
+      exit 1
+    fi
+    sudo rm $1
+    cerr
+  fi
+}
+
+delete_link $fan_include_path
+delete_link $fan_lib_path
+
+sudo ln -s $(realpath fan) $fan_include_path
+cerr
+sudo ln -s $(realpath lib/fan) $fan_lib_path
+cerr
