117b2c1f1e00221141332ed4d21503f775d09d0d
6413 <56801084+6413@users.noreply.github.com>
2024-11-15 16:19:56 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git clone https://github.com/7244/WITCH.git
+mv WITCH /usr/local/include/WITCH
+
+git clone https://github.com/7244/BCOL.git
+mv BCOL /usr/local/include/BCOL
+
+git clone https://github.com/7244/BLL.git
+mv BLL /usr/local/include/BLL
