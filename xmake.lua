0e279bb5e00b9c2b923adf7e38be925bd919bb67
6413 <56801084+6413@users.noreply.github.com>
2026-04-04 21:13:41 +0300

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -465,18 +465,25 @@ target("a.exe")
   end
 
   on_load(function (target)
-    if target:is_plat("linux") and has_config("FAN_GUI") then
+    if target:is_plat("linux") then
       import("lib.detect.find_tool")
-      local pkg_config = find_tool("pkg-config")
-      if pkg_config then
-        local libs = os.iorunv("pkg-config", {"--libs", "gtk+-3.0"})
-        if libs then
-          for _, lib in ipairs(libs:split("%s+")) do
-            if lib:startswith("-l") then
-              target:add("links", lib:sub(3))
+      if has_config("FAN_GUI") then
+        local pkg_config = find_tool("pkg-config")
+        if pkg_config then
+          local libs = os.iorunv("pkg-config", {"--libs", "gtk+-3.0"})
+          if libs then
+            for _, lib in ipairs(libs:split("%s+")) do
+              if lib:startswith("-l") then
+                target:add("links", lib:sub(3))
+              end
             end
           end
         end
+      end
+      if find_tool("mold") then
+        target:add("ldflags", "-fuse-ld=mold", {force = true})
+      elseif find_tool("gold") then
+        target:add("ldflags", "-fuse-ld=gold", {force = true})
       end
     end
   end)
