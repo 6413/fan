49c8e6f27fffe62ab3852b9d54fc7845f8a3340c
6413 <56801084+6413@users.noreply.github.com>
2026-04-18 21:51:23 +0300

    Update renderer0_impl.cpp

--- fan/graphics/gui/tilemap_editor/renderer0_impl.cpp
+++ fan/graphics/gui/tilemap_editor/renderer0_impl.cpp
@@ -32,7 +32,7 @@ namespace fan::graphics {
 
   tilemap_renderer_t::id_t tilemap_renderer_t::open_map(std::string_view map_name, const properties_t& p, const std::source_location& callers_path) {
     std::string smap_name(map_name);
-    compiled_map_t* compiled = compile(smap_name, fan::io::file::strip_extension(smap_name), callers_path);
+    compiled_map_t* compiled = compile(fan::io::file::strip_extension(smap_name), smap_name, callers_path);
     return add(compiled, p);
   }
 
