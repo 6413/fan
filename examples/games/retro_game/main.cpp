42825aa50901c3d1923d16988e48f53e37dca0dc
6413 <56801084+6413@users.noreply.github.com>
2026-05-22 19:45:33 +0300

    Update main.cpp

--- examples/games/retro_game/main.cpp
+++ examples/games/retro_game/main.cpp
@@ -7,59 +7,43 @@ struct pile_t : engine_t, fan::frame_task_t<pile_t> {
 
   struct level1_t : fan::stage_t<level1_t> {
     void open(void* sod) {
-      id = pile.renderer.open_map("sample_level.fte", {
+      map = tilemap_instance_t(pile.renderer, "sample_level.fte", {
         .position = pile.player.body.get_position(),
         .size = fan::vec2i(16, 9) * 5.f,
       });
 
-      pile.renderer.iterate_tiles(id, [&](const auto& tile) {
-        collisions.emplace_back(physics->create_box(tile.position, tile.size, 0, fan::physics::body_type_e::static_body, {}));
-      });
+      map.build_collisions();
 
-      fan::vec2 ts = pile.renderer.get_tile_size(id);
-      pile.renderer.setup_view(id, pile.player.body, pile.ic, 2.08f);
+      fan::vec2 ts = pile.renderer.get_tile_size(map.id);
+      pile.renderer.setup_view(map.id, pile.player.body, pile.ic, 2.08f);
 
-      pile.renderer.iterate_marks(id, {
+      pile.renderer.iterate_marks(map.id, {
         {"key", [&](auto& m) {
-          key = {{
+          key.open(pile.player.body, {{
             .position = m.position, .size = 32.f,
             .image = {"images/key.webp", image_presets::pixel_art()},
             .shape_properties{.is_sensor = true}
-          }};
-          key.on_sensor_enter(pile.player.body, [&] { key.erase(); });
+          }}, [](physics::sprite_t& s) { s.erase(); });
         }},
         {"door", [&](auto& m) {
           fan::vec2 size{32.f / 6.f, 64.f};
-          door = {{
+          door.open(pile.player.body, {{
             .position = m.position.offset_y(-size.y + ts.y), .size = size,
             .image = {"images/door_closed.png", image_presets::pixel_art()},
             .shape_properties{.is_sensor = true}
-          }};
-          door_pos = door.get_position();
-
-          door.on_sensor_enter(pile.player.body, [&] {
-            if (!door_open) {
-              door.set_position(door_pos.offset_x(-32.f));
-              door.set_size({32.f, 64.f});
-              door.set_image({"images/door.png", image_presets::pixel_art()});
-              door_open = true;
-            }
+          }}, [](physics::sprite_t& s) {
+            s.set_position(s.get_position().offset_x(-32.f));
+            s.set_size({32.f, 64.f});
+            s.set_image({"images/door.png", image_presets::pixel_art()});
           });
         }},
       });
     }
-    void close() {
-      for (auto& c : collisions) { c.destroy(); }
-      pile.renderer.close_map(id); 
-    }
 
-    void update() {
-      pile.renderer.update(id, pile.player.body.get_position());
-    }
+    void update() { map.update(pile.player.body.get_position()); }
 
-    std::vector<fan::physics::entity_t> collisions;
-    tilemap_renderer_t::id_t id;
-    physics::sprite_t key, door;
+    tilemap_instance_t map;
+    trigger_t key, door;
     fan::vec2 door_pos = 0;
     bool door_open = false;
   };
