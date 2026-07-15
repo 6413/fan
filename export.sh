aceaf36107324453fdcdfe934ec5645a1b5104a0
6413 <56801084+6413@users.noreply.github.com>
2026-07-15 22:04:15 +0300

    Up

--- export.sh
+++ export.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+
 script_dir="$(dirname "$0")"
+
 input_exe="$1"
 [ -z "$input_exe" ] && input_exe="fan.exe"
-base="$(basename "$input_exe")"
-base_noext="${base%.*}"
-read -p "Output folder name [$base_noext]: " outname
-[ -z "$outname" ] && outname="$base_noext"
-outdir="$script_dir/$outname"
+
+base="$(basename "$input_exe" .exe)"
+
+read -p "New exe name [$base]: " newname
+[ -z "$newname" ] && newname="$base"
+
+outdir="$script_dir/$newname"
+
 python3 "$script_dir/export.py" "$input_exe" "$outdir" --force
-mv "$outdir/$base" "$outdir/$outname.exe"
+
+mv "$outdir/$(basename "$input_exe")" "$outdir/$newname.exe"
